1859a55e0f8e3fbabb49ed7e172ed14b7513664c
Efe Ertug Erdem <efeerdem20@ku.edu.tr>
2024-11-09 16:53:52 +0300

    File inclusion security check fix

--- internal/infrastructure/filestorage.go
+++ internal/infrastructure/filestorage.go
@@ -1,11 +1,12 @@
 package infrastructure
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/mechatron-x/atehere/internal/config"
-	"github.com/mechatron-x/atehere/internal/core"
 )
 
 type DiskFileSaver struct{}
@@ -13,15 +14,20 @@ type DiskFileSaver struct{}
 func (dfs DiskFileSaver) Save(filename string, data []byte, conf config.Api) error {
 	savePath := filepath.Join(conf.StaticRoot, filepath.Clean(filename))
 
+	savePath = filepath.Clean(savePath)
+	if !strings.HasPrefix(savePath, conf.StaticRoot) {
+		return fmt.Errorf("invalid static root")
+	}
+
 	imageFile, err := os.Create(savePath)
 	if err != nil {
-		return core.NewPersistenceFailureError(err)
+		return err
 	}
 	defer imageFile.Close()
 
 	err = os.WriteFile(savePath, data, 0600)
 	if err != nil {
-		return core.NewPersistenceFailureError(err)
+		return err
 	}
 
 	return nil
